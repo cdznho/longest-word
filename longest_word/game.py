a6b671d94f220cc474f3005566964c420d20c19b
cedricdeschaut1@gmail.com <cedricdeschaut1@gmail.com>
2023-10-08 12:24:35 +0000

    Small web app wrapper around the Game

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,5 +1,6 @@
 import string
 import random
+import requests
 
 class Game:
     def __init__(self):
@@ -7,6 +8,14 @@ class Game:
         for _ in range(9):
             self.grid.append(random.choice(string.ascii_uppercase))
 
-    def is_valid(self, word: str) -> bool:
-        """Return True if and only if the word is valid, given the Game's grid"""
-        pass # TODO
+    def is_valid(self, word):
+        # [...]
+
+        return self.__check_dictionary(word)
+
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
